30ec7ff74871f10c8d9ac17f7b88f076c6058b7c
Grant Galitz <grantgalitz@gmail.com>
2015-07-12 18:58:04 -0400

    Adding files
    Just wanted to clear out my account due to account snoopers.

---
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -0,0 +1,194 @@
+"use strict";
+/*
+ Copyright (C) 2012-2015 Grant Galitz
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+var Iodine = null;
+var Blitter = null;
+var Mixer = null;
+var MixerInput = null;
+var timerID = null;
+window.onload = function () {
+    //Initialize Iodine:
+    Iodine = new GameBoyAdvanceEmulator();
+    //Initialize the graphics:
+    registerBlitterHandler();
+    //Initialize the audio:
+    registerAudioHandler();
+    //Register the save handler callbacks:
+    registerSaveHandlers();
+    //Hook the GUI controls.
+    registerGUIEvents();
+}
+function registerBlitterHandler() {
+    Blitter = new GlueCodeGfx();
+    Blitter.attachCanvas(document.getElementById("emulator_target"));
+    Iodine.attachGraphicsFrameHandler(function (buffer) {Blitter.copyBuffer(buffer);});
+}
+function registerAudioHandler() {
+    Mixer = new GlueCodeMixer();
+    MixerInput = new GlueCodeMixerInput(Mixer);
+    Iodine.attachAudioHandler(MixerInput);
+    Iodine.enableAudio();
+}
+function registerGUIEvents() {
+    addEvent("keydown", document, keyDown);
+    addEvent("keyup", document, keyUpPreprocess);
+    addEvent("change", document.getElementById("rom_load"), fileLoadROM);
+    addEvent("change", document.getElementById("bios_load"), fileLoadBIOS);
+    addEvent("click", document.getElementById("play"), function (e) {
+        Iodine.play();
+        this.style.display = "none";
+        document.getElementById("pause").style.display = "inline";
+        if (e.preventDefault) {
+             e.preventDefault();
+        }
+    });
+    addEvent("click", document.getElementById("pause"), function (e) {
+        Iodine.pause();
+        this.style.display = "none";
+        document.getElementById("play").style.display = "inline";
+        if (e.preventDefault) {
+             e.preventDefault();
+        }
+    });
+    addEvent("click", document.getElementById("restart"), function (e) {
+        Iodine.restart();
+        if (e.preventDefault) {
+             e.preventDefault();
+        }
+    });
+    document.getElementById("sound").checked = true;
+    addEvent("click", document.getElementById("sound"), function () {
+        if (this.checked) {
+            Iodine.enableAudio();
+        }
+        else {
+            Iodine.disableAudio();
+        }
+    });
+    document.getElementById("skip_boot").checked = false;
+    addEvent("click", document.getElementById("skip_boot"), function () {
+             if (this.checked) {
+                Iodine.enableSkipBootROM();
+             }
+             else {
+                Iodine.disableSkipBootROM();
+             }
+    });
+    document.getElementById("toggleSmoothScaling").checked = true;
+    addEvent("click", document.getElementById("toggleSmoothScaling"), function () {
+             if (Blitter) {
+                Blitter.setSmoothScaling(this.checked);
+             }
+    });
+    document.getElementById("toggleDynamicSpeed").checked = true;
+    addEvent("click", document.getElementById("toggleDynamicSpeed"), function () {
+             if (this.checked) {
+                Iodine.enableDynamicSpeed();
+             }
+             else {
+                Iodine.disableDynamicSpeed();
+             }
+    });
+    addEvent("change", document.getElementById("import"), function (e) {
+             if (typeof this.files != "undefined") {
+                try {
+                    if (this.files.length >= 1) {
+                        writeRedTemporaryText("Reading the local file \"" + this.files[0].name + "\" for importing.");
+                        try {
+                            //Gecko 1.9.2+ (Standard Method)
+                            var binaryHandle = new FileReader();
+                            binaryHandle.onload = function () {
+                                if (this.readyState == 2) {
+                                    writeRedTemporaryText("file imported.");
+                                    try {
+                                        import_save(this.result);
+                                    }
+                                    catch (error) {
+                                        writeRedTemporaryText(error.message + " file: " + error.fileName + " line: " + error.lineNumber);
+                                    }
+                                }
+                                else {
+                                    writeRedTemporaryText("importing file, please wait...");
+                                }
+                            }
+                            binaryHandle.readAsBinaryString(this.files[this.files.length - 1]);
+                        }
+                        catch (error) {
+                            //Gecko 1.9.0, 1.9.1 (Non-Standard Method)
+                            var romImageString = this.files[this.files.length - 1].getAsBinary();
+                            try {
+                                import_save(romImageString);
+                            }
+                            catch (error) {
+                                writeRedTemporaryText(error.message + " file: " + error.fileName + " line: " + error.lineNumber);
+                            }
+                        }
+                    }
+                    else {
+                        writeRedTemporaryText("Incorrect number of files selected for local loading.");
+                    }
+                }
+                catch (error) {
+                    writeRedTemporaryText("Could not load in a locally stored ROM file.");
+                }
+             }
+             else {
+                writeRedTemporaryText("could not find the handle on the file to open.");
+             }
+             if (e.preventDefault) {
+                e.preventDefault();
+             }
+    });
+    addEvent("click", document.getElementById("export"), refreshStorageListing);
+    addEvent("unload", window, ExportSave);
+    Iodine.attachSpeedHandler(function (speed) {
+        var speedDOM = document.getElementById("speed");
+        speedDOM.textContent = "Speed: " + speed;
+    });
+    //setInterval(ExportSave, 60000); //Do periodic saves.
+}
+function resetPlayButton() {
+    document.getElementById("pause").style.display = "none";
+    document.getElementById("play").style.display = "inline";
+}
+function lowerVolume() {
+    Iodine.incrementVolume(-0.04);
+}
+function raiseVolume() {
+    Iodine.incrementVolume(0.04);
+}
+function writeRedTemporaryText(textString) {
+    if (timerID) {
+        clearTimeout(timerID);
+    }
+    document.getElementById("tempMessage").style.display = "block";
+    document.getElementById("tempMessage").textContent = textString;
+    timerID = setTimeout(clearTempString, 5000);
+}
+function clearTempString() {
+    document.getElementById("tempMessage").style.display = "none";
+}
+//Some wrappers and extensions for non-DOM3 browsers:
+function addEvent(sEvent, oElement, fListener) {
+    try {    
+        oElement.addEventListener(sEvent, fListener, false);
+    }
+    catch (error) {
+        oElement.attachEvent("on" + sEvent, fListener);    //Pity for IE.
+    }
+}
+function removeEvent(sEvent, oElement, fListener) {
+    try {    
+        oElement.removeEventListener(sEvent, fListener, false);
+    }
+    catch (error) {
+        oElement.detachEvent("on" + sEvent, fListener);    //Pity for IE.
+    }
+}
