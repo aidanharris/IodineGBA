0c4a70ba9b1a172f765b54f1d6f0c8698dc334af
Grant Galitz <grantgalitz@gmail.com>
2015-08-22 02:51:14 -0400

    Year 2038 Safety
    Might as well use unsigned instead of signed int time logic...

--- IodineGBA/core/Emulator.js
+++ IodineGBA/core/Emulator.js
@@ -92,7 +92,7 @@ GameBoyAdvanceEmulator.prototype.restart = function () {
 }
 GameBoyAdvanceEmulator.prototype.timerCallback = function (lastTimestamp) {
     //Callback passes us a reference timestamp:
-    this.lastTimestamp = lastTimestamp | 0;
+    this.lastTimestamp = lastTimestamp >>> 0;
     if (!this.paused) {
         if (!this.faultFound && this.loaded) {                          //Any error pending or no ROM loaded is a show-stopper!
             this.iterationStartSequence();                              //Run start of iteration stuff.
@@ -195,7 +195,7 @@ GameBoyAdvanceEmulator.prototype.invalidateMetrics = function () {
 }
 GameBoyAdvanceEmulator.prototype.resetMetrics = function () {
     this.clockCyclesSinceStart = 0;
-    this.metricStart = this.lastTimestamp | 0;
+    this.metricStart = this.lastTimestamp >>> 0;
 }
 GameBoyAdvanceEmulator.prototype.calculateTimings = function () {
     this.clocksPerSecond = this.settings.emulatorSpeed * 0x1000000;
@@ -215,8 +215,8 @@ GameBoyAdvanceEmulator.prototype.setIntervalRate = function (intervalRate) {
     }
 }
 GameBoyAdvanceEmulator.prototype.calculateSpeedPercentage = function () {
-    if ((this.metricStart | 0) > 0) {
-        var timeDiff = Math.max((this.lastTimestamp | 0) - (this.metricStart | 0), 1);
+    if ((this.metricStart >>> 0) != 0) {
+        var timeDiff = Math.max((this.lastTimestamp >>> 0) - (this.metricStart >>> 0), 1) | 0;
         if ((timeDiff | 0) >= (this.settings.metricCollectionMinimum | 0)) {
             if (this.speedCallback) {
                 var result = ((this.clockCyclesSinceStart | 0) * 100000) / ((timeDiff | 0) * 0x1000000);
